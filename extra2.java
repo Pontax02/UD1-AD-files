49b929d7d4d2b480836932dde304f3e04202c209
a22ricardoadc <a22ricardoadc@iesteis.es>
2025-09-18 13:32:49 +0200

    avanzando en el ejercicio extra 2

--- extra2.java
+++ extra2.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,15 +26,31 @@ public class extra2 {
     }
 
     public static void motor(String comando) {
-        String[] comandoSeparado = comando.split(" ");
+        String[] comandoSeparado = comando.split(" ", 2);
+        String orden = comandoSeparado[0];
+        String argumentos = (comandoSeparado.length > 1) ? comandoSeparado[1] : "";
 
-        switch (comandoSeparado[0]) {
+        switch (orden) {
+            case "cat" -> {
+                if (comandoSeparado.length == 2) {
+                    cat(argumentos);
+                } else {
+                    System.err.println("SINTAXIS: cat <archivo>");
+                }
+            }
+
             case "ls" -> {
-
+                if (comandoSeparado.length == 2) {
+                    ls(argumentos.replace("\"", ""));
+                } else if (comandoSeparado.length == 1) {
+                    ls(directorioTrabajo);
+                } else {
+                    System.err.println("SINTAXIS: ls [directorio]");
+                }
             }
             case "cd" -> {
                 if (comandoSeparado.length == 2) {
-                    cd(comandoSeparado[1]);
+                    cd(argumentos);
                 } else {
                     System.err.println("Argumentos invalido");
                 }
@@ -45,6 +63,15 @@ public class extra2 {
         }
     }
 
+    private static void cat(String ficheiro) {
+        Path f = Paths.get(ficheiro);
+        if (!Files.isReadable(f)) {
+            System.err.println("Arquivo non lexibel");
+        } else {
+
+        }
+    }
+
     public static void cd(String directorio) {
         Path dir = Paths.get(directorio);
         // comprobamos primero q es un directorio
@@ -53,6 +80,57 @@ public class extra2 {
                 directorioTrabajo = directorio;
             } else {
                 directorioTrabajo = directorioTrabajo + "/" +  directorio;
+            }
+        } else {
+            System.err.println("No es un directorio");
+        }
+    }
+
+    public static void ls (String pathString) {
+        Path dir = Path.of(pathString);
+        if (Files.isDirectory(dir)) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+
+                for (Path archivo: stream) {
+
+                    String linha = "";
+                    Boolean isDir = false;
+                    // es un directorio?
+                    if (Files.isDirectory(archivo)) {
+                        isDir = true;
+                        linha += "d";
+                    } else {
+                        linha += "-";
+                    }
+                    // permisos d lectura?
+                    if (Files.isReadable(archivo)) {
+                        linha += "r";
+                    } else {
+                        linha += "-";
+                    }
+                    // permisos d escritura?
+                    if (Files.isWritable(archivo)) {
+                        linha += "w";
+                    } else {
+                        linha += "-";
+                    }
+                    // permisos d ejecucion
+                    if (Files.isExecutable(archivo)) {
+                        linha += "x";
+                    } else {
+                        linha += "-";
+                    }
+                    // nombre del archivo
+                    linha += " " +  archivo.getFileName();
+                    if (isDir) {
+                        linha+="/";
+                    }
+
+                    System.out.println(linha);
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         } else {
             System.err.println("No es un directorio");
